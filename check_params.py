90a16883cb013fed40a29f4f8664ac18a0a8ef0f
UltralyticsAssistant <web@ultralytics.com>
2026-05-15 04:32:03 +0000

    Auto-format by https://ultralytics.com/actions

--- check_params.py
+++ check_params.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # 加载模型 (这里以 yolov8n.pt 为例，如果是你自己的模型，请替换为模型路径，例如 'runs/detect/train/weights/best.pt')
-model = YOLO('Pothole_CBAM_Project\\exp_cbam\\weights\\best.pt')
+model = YOLO("Pothole_CBAM_Project\\exp_cbam\\weights\\best.pt")
 
 # 打印模型信息
 print("简单信息:")
