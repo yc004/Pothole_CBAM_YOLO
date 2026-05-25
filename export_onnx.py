fefd91c5fc9e6983efefa9d30bac93dfeb0b1f06
UltralyticsAssistant <web@ultralytics.com>
2026-05-25 05:04:03 +0000

    Auto-format by https://ultralytics.com/actions

--- export_onnx.py
+++ export_onnx.py
@@ -1,10 +1,11 @@
-from ultralytics import YOLO
 import os
+
+from ultralytics import YOLO
 
 # 定义模型路径列表
 model_paths = [
     r"Pothole_Baseline_Project/exp_baseline/weights/best.pt",  # 基线版本
-    r"Pothole_CBAM_Project/exp_cbam/weights/best.pt"           # 改进版本
+    r"Pothole_CBAM_Project/exp_cbam/weights/best.pt",  # 改进版本
 ]
 
 for path in model_paths:
@@ -12,12 +13,7 @@ for path in model_paths:
         print(f"\n正在处理模型: {path}")
         try:
             model = YOLO(path)
-            model.export(
-                format="onnx",
-                imgsz=640,
-                opset=12,
-                simplify=True
-            )
+            model.export(format="onnx", imgsz=640, opset=12, simplify=True)
             print(f"成功导出: {path}")
         except Exception as e:
             print(f"导出失败 {path}: {e}")
